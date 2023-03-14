32a27be5445dbbb0f2dea511e25b8ed1937f901b
Hajin <hajin1819@gmail.com>
2023-03-14 15:53:14 +0000

    Project completely replicated

--- keyboard/print.h
+++ keyboard/print.h
@@ -13,14 +13,16 @@ void printMiddle(char c, string COLOR) { cout << COLOR << "| " << c << " |" << R
 void printBottom(string COLOR) { cout << COLOR << " --- " << RESET; }
 void printKeyboard(vector<vector<char>> keyboard, vector<vector<string>> colors) {
     for (int i = 0; i < keyboard.size(); i++) {
+        string spaces((50 - (keyboard[i].size() * 5))/2, ' ');
+        cout << spaces;
         for (int j = 0; j < keyboard[i].size(); j++) {
             printTop(colors[i][j]);
         }
-        cout << endl;
+        cout << endl << spaces;
         for (int j = 0; j < keyboard[i].size(); j++) {
             printMiddle(keyboard[i][j], colors[i][j]);
         }
-        cout << endl;
+        cout << endl << spaces;
         for (int j = 0; j < keyboard[i].size(); j++) {
             printBottom(colors[i][j]);
         }
